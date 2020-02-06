7b556d19a7081ed07b87f60dfaf34558f45bba1a
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2020-02-06 02:09:26 -0800

    debug analysis

--- analyze.mjs
+++ analyze.mjs
@@ -4,21 +4,24 @@
 //find the unique unit types in an object
 export function analyzeUnitTypes({features}){
   const res = new Map() // map of the analysise results
-  const template = {
+  const getTemplate = () => ({
     count: 0,
     unitnames: [] 
-  } // each result should include some predetermined infos
+  }) // each result should include some predetermined infos
+  const addRecord = (res, UNIT_TYPE, UNIT_NAME) => {
+    const o = res.get(UNIT_TYPE)
+    o.unitnames.push(UNIT_NAME)
+    o.count++
+  }
   for (let featureentry of features){
     const {UNIT_TYPE, UNIT_NAME} = featureentry.properties
     if(UNIT_TYPE && !res.has(UNIT_TYPE)){ // don't allow empty strings
-      res.set(UNIT_TYPE, template)
-      const o = res.get(UNIT_TYPE)
-      o.unitnames.push(UNIT_NAME)
+      res.set(UNIT_TYPE, getTemplate())
+      addRecord(res, UNIT_TYPE, UNIT_NAME)
     } else if (res.has(UNIT_TYPE)){
-      const o = res.get(UNIT_TYPE)
-      o.unitnames.push(UNIT_NAME)
+      addRecord(res, UNIT_TYPE, UNIT_NAME)
     } 
     
   }
-  return res // get rid of any empty strings
+  return res
 }

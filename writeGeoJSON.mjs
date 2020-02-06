afa4566438d3318c08e1c4c8b59caa48cc29128b
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2020-02-06 02:10:54 -0800

    a tool to write the modified GeoJSON output to a file

---
+++ writeGeoJSON.mjs
@@ -0,0 +1,47 @@
+
+import {join, format} from 'path'
+import {promises as fsp} from 'fs'
+
+const datestr = (d, joiner='_') => [...new Map([
+  ['getUTCFullYear', res => res], 
+  ['getUTCMonth', res => `${res}`.padStart(2, '0')], 
+  ['getUTCDay', res => `${res}`.padStart(2, '0')], 
+  ['getUTCHours', res => `${res}`.padStart(2, '0')], 
+  ['getUTCMinutes', res => `${res}`.padStart(2, '0')], 
+  ['getUTCSeconds', res => `${res}`.padStart(2, '0')], 
+  ['getTimezoneOffset', (res, hrs = res/60) => `${(Math.sign(res)===1) ? '-' : '+'}`+`${hrs}`.padStart(2, '0')] 
+]).entries()]
+  .map(([fname, fmtfn]) => fmtfn(d[fname]()))
+  .join(joiner)
+
+const defaults = {
+  outdir: 'sanitized',
+}
+
+export async function writeGeoJSON(geojson, outname = datestr(new Date()), outdir = defaults.outdir ) {
+  const ext = '.geojson'
+  const target = format({
+    dir: outdir,
+    name: outname,
+    ext
+  })
+  let filehandle = null
+  let data = null
+  try {
+    data = await JSON.stringify(geojson)
+    await fsp.open(target, 'wx')
+      .then(fh => {
+        filehandle = fh
+        return fh
+      })
+      .then(fh => fsp.writeFile(
+        fh, data
+      ))
+  } catch (e) {
+    throw e
+  } finally {
+    if (filehandle) {
+      await filehandle.close()
+    }
+  }
+}
